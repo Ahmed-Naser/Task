85a3dfd2b25991156b8ce9b14ea7786270f26d7e
Ahmed Naser <ahmed.n.elbahnasy@gmail.com>
2019-03-06 23:50:50 +0200

    adddd

---
+++ Task/Task/Infrastructure/Mapper/AdminMapperConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Task.Core.Domain;
+using Task.Core.Infrastructure.Mapper;
+using Task.Models;
+
+namespace Task.Infrastructure.Mapper
+{
+    public class AdminMapperConfiguration : Profile, IOrderedMapperProfile
+    {
+        protected virtual void CreateCatalogMaps()
+        {
+            CreateMap<Product, ProductModel>().ReverseMap();
+        }
+
+        #region Properties
+        /// <summary>
+        /// Order of th
+        public int Order => 0;
+        #endregion
+    }
+}
